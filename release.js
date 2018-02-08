7b88179baa2aa76122a5c8aadb9675e2edc8b61d
lon <longhaohe@gmail.com>
2018-02-08 23:50:36 +0800

    Output prompt first

--- release.js
+++ release.js
@@ -3,15 +3,15 @@ const exec = require('child_process').exec
 function release (pkg) {
     const version = 'v' + pkg.version
     const sh = [
+        'echo [1/3]: commit',
         'git add package.json',
         `git commit -m "Bump to ${version}"`,
         'git push',
-        'echo [1/3]: commit',
+        'echo [2/3]: tag',
         `git tag ${version}`,
         `git push origin ${version}`,
-        'echo [2/3]: tag',
-        'npm publish',
         'echo [3/3]: publish',
+        'npm publish',
     ].join(' && ')
     exec(sh, err => {
         if (err) {
