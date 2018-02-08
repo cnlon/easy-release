a1a854551a66d16471c268cfebea8d7e1a30223f
lon <cnlon@users.noreply.github.com>
2018-02-08 22:59:56 +0800

    Update release.js

--- release.js
+++ release.js
@@ -3,10 +3,8 @@ const exec = require('child_process').exec
 function release (pkg) {
     const version = 'v' + pkg.version
     const sh = [
-        'git checkout master',
         'npm publish',
         'echo [1/3]: npm publish',
-        'git add -A',
         `git commit -m "Bump to ${version}"`,
         'git push',
         'echo [2/3]: git commit',
